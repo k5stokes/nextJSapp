0d5261320547bb923164bb2928ab6501741d64fa
Kris Stokes <kris.stokes@bonddigital.com>
2026-06-13 17:18:39 -0500

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google';
+import { Bakbak_One } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
+
+export const bakbakOne = Bakbak_One({
+  subsets: ['latin'],
+  weight: '400'
+});
