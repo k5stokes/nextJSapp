305d999137ae116744d5fe16578fd036a7e8e53b
Kris Stokes <kris.stokes@bonddigital.com>
2026-06-13 23:10:46 -0500

    replaced all lusitana font with bakbakOne

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,5 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-//import { lusitana } from '@/app/ui/fonts';
+//import { bakbakOne } from '@/app/ui/fonts';
 import { bakbakOne } from './fonts';
 
 export default function AcmeLogo() {
